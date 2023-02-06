36ab1cc00f03b8424747243d72c4aee00d5d9796
David Bushell <david@dbushell.com>
2023-02-06 09:07:51 +0000

    traefik config

--- README.md
+++ README.md
@@ -50,4 +50,4 @@ See the [Traefik documentation](https://doc.traefik.io/traefik/providers/docker/
 
 * * *
 
-[MIT License](/LICENSE) | Copyright © 2021 [David Bushell](https://dbushell.com) | [@dbushell](https://twitter.com/dbushell)
+[MIT License](/LICENSE) | Copyright © 2023 [David Bushell](https://dbushell.com) | [@dbushell](https://twitter.com/dbushell)
---
+++ config/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+TRFK_DIR="/etc/traefik"
+TRFK_TEMPLATE="$TRFK_DIR/template.toml"
+TRFK_CONFIG="$TRFK_DIR/traefik.toml"
+TRFK_CONFIG_NOTICE="#\n# Warning!\n# This file is auto-generated and will be overwritten!\n#\n\n"
+
+echo "Generating Traefik config: $TRFK_CONFIG"
+
+{
+  apk add gettext
+  envsubst < $TRFK_TEMPLATE > $TRFK_CONFIG
+} &> /dev/null
+
+echo -e "$TRFK_CONFIG_NOTICE$(cat $TRFK_CONFIG)" > $TRFK_CONFIG
+
+exec traefik
